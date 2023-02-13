ba40304d5dc99bc3364e8005b0336cf9b4f76b6e
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-02-12 16:53:00 -0800

    add search bar component

--- src/componenets/Sidebar.jsx
+++ src/componenets/Sidebar.jsx
@@ -4,6 +4,7 @@ import ChatIcon from "@mui/icons-material/Chat";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import { Avatar, IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 function Sidebar() {
   return (
     <div className="sidebar">
@@ -19,6 +20,13 @@ function Sidebar() {
           <IconButton>
             <MoreVertIcon />
           </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar_search">
+        <div className="sidebar_searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Serach or start new chat" />
         </div>
       </div>
     </div>
