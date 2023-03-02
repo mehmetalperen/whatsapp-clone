924ac18dfcda5bfe38f2ae787c83692f0b2d0064
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-03-02 11:58:20 -0800

    add listener for the pusher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./componenets/Chat";
 import Sidebar from "./componenets/Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    //listener for the pusher
+    const pusher = new Pusher("b23f5e99ce5b8bebe530", {
+      cluster: "us3",
+    });
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
   return (
     <div className="app">
       <div className="app_body">
