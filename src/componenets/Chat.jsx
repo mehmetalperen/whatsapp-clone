f2a440ae2dcd440e9c4a55384462dd166887832f
Mehmet Nadi <mhmtalperennadi@gmail.com>
2023-03-02 13:51:03 -0800

    finish whatsapp clone

--- src/componenets/Chat.jsx
+++ src/componenets/Chat.jsx
@@ -1,13 +1,26 @@
 import { Avatar, Icon, IconButton } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import SearchIcon from "@mui/icons-material/Search";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import "./Chat.css";
 import MicIcon from "@mui/icons-material/Mic";
+import axios from "../axios";
 
 function Chat({ messages }) {
+  const [newMsg, setNewMsg] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    await axios.post("/api/messages/new", {
+      message: newMsg,
+      name: "You",
+      timestamp: "just now",
+      received: true,
+    });
+    setNewMsg("");
+  };
   return (
     <div className="chat">
       <div className="chat_header">
@@ -49,8 +62,15 @@ function Chat({ messages }) {
         </IconButton>
 
         <form>
-          <input type="text" placeholder="Type a message" />
-          <button type="submit">Send a message</button>
+          <input
+            value={newMsg}
+            onChange={(e) => setNewMsg(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
